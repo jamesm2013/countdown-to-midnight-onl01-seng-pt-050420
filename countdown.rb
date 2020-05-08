1ffd2495b54963be0645ebee96e59051154e03a4
james Johnson <2013matthew2013@gmail.com>
2020-05-08 14:20:04 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
-#write your code here
+def countdown(number)
+  while number >0
+  puts "#{number} second(s)!"
+  number -=1
+end
+  return "HAPPY NEW YEAR"
 
-def countdown
+def countdown_with_sleep(num)
+   sleep (5)
+  end
 end
